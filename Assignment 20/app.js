ada382687f87ddf06552e0b40474b67514e31f7b
nofil <gnick0137@gmail.com>
2024-04-27 09:12:05 -0700

    frist commit

---
+++ Assignment 20/app.js
@@ -0,0 +1,62 @@
+// let myArray1= []
+// var myArray2=[""]
+// var myArray3=["Array","bon"]
+// var myArray4=[1,2]
+// var myArray5=[true,false]
+// var myArray6=[true,2,"array"]
+
+
+
+// var arr=[
+//     "SSC",
+//     "HSC",
+//     "BCS",
+//     "BS,",
+//     "BCOM",
+//     "MS",
+//     "M",
+//     "Phil",
+//     "PhD",
+// ]
+// for(var i=1;i< arr.length;i++){
+//     document.write(`${i}) ${arr[i]} <br>`)  
+// }
+
+// var catArr = ["This", "is", "my", "cat"]
+// document.write(catArr.join(" "))
+
+
+
+
+
+// const scores = [420, 380, 450];
+
+
+// scores.sort((a, b) => a - b);
+
+
+// document.write(scores);
+
+
+
+
+
+// const studentNames = ['Alice', 'Bob', 'Charlie'];
+
+
+// const scores = [420, 380, 450];
+
+
+// const percentages = scores.map(score => (score / 500) * 100);
+
+
+// for (let i = 0; i < studentNames.length; i++) {
+//   document.write(`${studentNames[i]} : Score - ${scores[i]}, Percentage - ${percentages[i]}%`);
+// }
+
+
+// const cities = ['New York', 'Los Angeles', 'Chicago', 'Houston', 'Phoenix'];
+
+// const selectedCities = cities.slice(0, 3);
+
+// console.log(selectedCities);
